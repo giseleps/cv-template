bddff17a84c6a72e834a754813a4dc9970c270ea
Narciso Benigno <narciso.benigno@gmail.com>
2024-02-03 10:56:50 +1100

    type the whole curriculum

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,95 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Gisele Pereira de Souza',
+  title: 'Receptionist and RSA certificated',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
-                </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'Residence': '<a href="https://www.google.com/maps/place/Miranda+NSW+2228/@-34.0331603,151.0998971,16z/data=!3m1!4b1!4m6!3m5!1s0x6b12b84572e5978d:0x5017d681632c180!8m2!3d-34.0299364!4d151.1055499!16zL20vMDJma3ht?entry=ttu"><i class="fa fa-home fact-icon"></i>Miranda - NSW</a>',
+    'Email': '<a href="mailto:gisele.pereira.souza@gmail.com"><i class="fa fa-envelope fact-icon"></i>gisele.pereira.souza@gmail.com</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 60],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 80],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['Receptionist', 100],
+    ['RSA', 100],
+    ['English', 60],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
+      title: 'Rydes Sydney Airport - Housekeeper',
+      period: 'Nov 2022 - Jun 2023',
+      skills: ['organisation', 'cleaning', 'client'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+Room Attendant
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
+      title: 'Nova Piramidal Thermoplastics - Relationship Coordinator (Contract)',
+      period: 'Abr 2016 – Nov 2016',
+      skills: ['negotiation', 'client'],
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+Addressed online support desk and phone tickets, by inserting orders into the ERP system from field sales representatives. Managed support requests within the company’s service level agreement, by ensuring all the tasks were completed timely and with quality. Created supported materials for training sessions to ensure standard procedures were followed when opening online tickets. Communicated with all clients weekly to provide regular updates on orders by email and phone calls
 `
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: 'Ramos and Zuanon Lawyers - Administrative Assistant',
+      period: 'Mar 2013 - Jan 2016',
+      skills: ['receptionist', 'facilities', 'accounts'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+Purchased stationery for the office, and administered all the company suppliers’ contracts, ensuring they were renewed on time and that they were achieved securely and in an organised manner. Received and sent correspondence, mainly accounting documents for monthly balance, ensuring they were sent on time. Assisted the accounts payable/receivable by issuing invoices to customers, performing bank reconciliation, and administering petty cash.
 `
-    }
+    },
+    {
+      title: 'Top Service (GPS Group) - Receptionist (Contract)',
+      period: 'Mar 2013 - Nov 2013',
+      skills: ['Microsoft office'],
+      contents: `
+Managed visitors, scheduled meetings, answered incoming calls and queries where possible. Supported the office management, by developing monthly costs reports on deliveries, taxis, and phone bills. Set up and cleaned up the boardroom/meeting room following all internal staff and client meetings. Printed, scanned, photocopied, and provided any office assistant and general support of office team needed.
+`
+    },
+    {
+      title: 'Stock Broker Investments S/A - Operational Assistant (Contract)',
+      period: 'Mar 2012 - Nov 2012',
+      skills: ['Microsoft office'],
+      contents: `
+Answered incoming calls from customers, providing support and solving doubts. Educated customers via online chat to use the home broker system and negotiations platforms.
+
+**Achievements**: Started as a receptionist and after 3 months was promoted to Operational Assistant.
+`
+    },
+    {
+      title: 'Process Outsourcing Services and Technology (Tivit) - Administrative Assistant/Receptionist (Contract)',
+      period: 'Nov 2011 – Jan 2012',
+      skills: [],
+      contents: `
+Managed monthly costs of taxis and correspondence courier, and prepared documentation for invoicing. Performed general office management tasks, organised reception area, quoted office materials, answered calls, dealt with the maintenance of the office.
+`
+    },
+    {
+      title: 'Alpha Shopping - Concierge Attendant',
+      period: 'Apr 2011 – Nov 2011',
+      skills: [],
+      contents: `
+Provided information for customers on directions and any doubts related to the centre. Answered phone calls, ensured customers were satisfied with the service.
+`
+    },
+    {
+      title: 'MC Donalds - Host',
+      period: 'Dec 2008 – Apr 2011',
+      skills: [],
+      contents: `
+Greeted customers on arrivals, organised events/birthdays, checked client’s satisfaction whilst they were having their meals. Performed general restaurant duties, cleaning, food preparation, serving meals and ensuring excellent customer service at all times.
+
+**Achievements**: Started as an attendant and was promoted to host in one year.
+`
+    },
   ],
   experience: [
     {
-      header: 'Performance',
+      header: 'Volunteer Work - Mardi Gras Parade 2021',
       skills: ['theater', 'play'],
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
+Acted as the official liaison and emergency contact between the entrant group and the event organiser, wearing the accreditation provided by the parade, and ensuring that the participants did not intentionally cause the parade to stop.
+
+Ensured safety of participants by remaining within the entry and vehicle areas throughout start marshalling, parade route and end pack down area.
+
 `
     },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
   ]
 };
